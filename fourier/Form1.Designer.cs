6b203195ba5d2328be31a7a2492cfc8e35e97156
Sohrscode <f_sohr@gmx.de>
2020-04-16 14:24:11 +0200

    asdf

--- fourier/Form1.Designer.cs
+++ fourier/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -39,14 +42,14 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
             this.label1.Location = new System.Drawing.Point(25, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(255, 29);
+            this.label1.Size = new System.Drawing.Size(269, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Furier Transformation";
+            this.label1.Text = "Fourier Transformation";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(30, 284);
+            this.panel1.Location = new System.Drawing.Point(30, 319);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(499, 436);
             this.panel1.TabIndex = 1;
@@ -60,17 +63,44 @@
             this.panel2.TabIndex = 2;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.AllowDrop = true;
+            this.trackBar1.Location = new System.Drawing.Point(178, 268);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(497, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            this.trackBar1.MouseCaptureChanged += new System.EventHandler(this.trackBar1_MouseCaptureChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label2.Location = new System.Drawing.Point(25, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "multiplier";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(843, 753);
+            this.ClientSize = new System.Drawing.Size(843, 838);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Furier Transformation";
+            this.Text = "Fourier Transformation";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +111,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- fourier/Form1.cs
+++ fourier/Form1.cs
@@ -61,7 +61,7 @@ namespace fourier
         }
         Point p2p(double x, double y)
         {
-            double xoffset = 20;
+            double xoffset = 60;
             double yoffset = 185;
             double factor = 20;
             return new Point((int)(xoffset + factor * x), (int)(yoffset + factor * -y));
@@ -69,20 +69,29 @@ namespace fourier
 
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
+
             Graphics g = panel2.CreateGraphics();
-            Brush b = new SolidBrush(Color.Red);
+            Brush b = new SolidBrush(Color.Black);
             Pen p = new Pen(Color.Black);
 
-            g.DrawLine(p, p2p(-35, 0), p2p(35, 0));
-            g.DrawLine(p, p2p(0, -9), p2p(0, 9));
+            FontFamily fontFamily = new FontFamily("Arial");
+            Font f = new Font(
+               fontFamily,
+               16,
+               FontStyle.Regular,
+               GraphicsUnit.Pixel);
 
-            g.DrawLine(p, p2p(35, 0), p2p(34.5, -0.5));
+            g.DrawLine(p, p2p(-35, 0), p2p(35, 0));   // x achse
+            g.DrawLine(p, p2p(0, -9), p2p(0, 9));    // y achse
+
+            g.DrawLine(p, p2p(35, 0), p2p(34.5, -0.5));  // x Pfeil
             g.DrawLine(p, p2p(35, 0), p2p(34.5, 0.5));
 
-            g.DrawLine(p, p2p(0, 9), p2p(0.5, 8.5));
+            g.DrawLine(p, p2p(0, 9), p2p(0.5, 8.5));   // y Pfeil
             g.DrawLine(p, p2p(0, 9), p2p(-0.5, 8.5));
 
-            g.DrawLine(p, p2p(-0.5, -0.5), p2p(0.5, 0.5));
+            g.DrawLine(p, p2p(-0.5, -0.5), p2p(0.5, 0.5));   // 0 linie
+
 
             for (double i = 0; i < 35; i += 1)
             {
@@ -94,17 +103,17 @@ namespace fourier
                 g.DrawLine(p, p2p(0.25,ii), p2p(-0.25, ii));
             }
 
-            // g.DrawLine(p, c2p2(3, 1)), c2p2(26, 8)));
+
 
             double y, x_old = 0, y_old = 0;
             double y2;
             double y3;
             bool is_first = true;
-            double periode = 5;
-            double periode2 = 4;
+            double periode = 1;
+            double periode2 = 2;
             double periode3 = 3;
             double hoehe = 4;
-            double multiplie = 0.5;
+            double multiplie = m_multiplier;
             for (double x = 0; x < 35/multiplie; x += 0.01)
             {
                 y = Math.Sin(2 * Math.PI*x/periode* 2);
@@ -121,6 +130,114 @@ namespace fourier
                     y_old = y + y2 + y3;
                 }
             }
+
+            String text = "0";
+            g.DrawString(text, f, b, p2p(-1, -0.25));
+
+            string[] ax = new string[10];
+            string[] ay = new string[10];
+            //   double[] ax2 = new double[10];
+
+            for(int i = 2; i < 9; i += 2)
+            {
+                double pos2 = (double)i / multiplie;
+                ay[i / 2] = pos2.ToString();
+
+                int length2 = ay[i / 2].Length;
+                double minus = 0.01;
+
+                if (length2 == 1)
+                {
+                    minus = 1;
+                }
+                else if (length2 == 2)
+                {
+                    minus = 1.4;
+                }
+                else if (length2 == 3)
+                {
+                    minus = 1.7;
+                }
+                else if (length2 == 4)
+                {
+                    minus = 1.9;
+                }
+                else if (length2 == 5)
+                {
+                    minus = 2.2;
+                }else if (length2 > 5)
+                {
+                    ay[i/2] = ay[i/2].Substring(0, 5);
+                    minus = 2.6;
+                }
+
+
+                g.DrawString(ay[i / 2], f, b, p2p(- minus,i+0.4));
+            }
+
+            for (int i = 5; i < 35; i += 5)
+            {
+                double pos = (double)i / multiplie;
+                ax[i / 5] = pos.ToString();
+
+                int length = ax[i / 5].Length;
+                double minus = 0.1;
+
+                if(length == 1)
+                {
+                    minus = 0.35;
+                }else if(length == 2)
+                {
+                    minus = 0.52;
+                }else if(length == 3)
+                {
+                    minus = 0.75;
+                }else if (length == 4)
+                {
+                minus = 1;
+                }
+                else if (length == 5)
+                {
+                    minus = 1.25;
+                } else if (length > 5)
+                {
+                    ax[i / 5] = ax[i / 5].Substring(0, 5);
+                    minus = 1.25;
+                }
+                g.DrawString(ax[i/5], f, b, p2p(i - minus, -0.2));
+            }
         }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // label2 =
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void trackBar1_Validated(object sender, EventArgs e)
+        {
+        }
+
+        private void trackBar1_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            double v = trackBar1.Value;
+            m_multiplier = v/10;
+            if (m_multiplier == 0)
+            {
+                m_multiplier = 0.1;
+            }else 
+          //  panel1.Refresh();
+            panel2.Refresh();
+        }
+        private double m_multiplier = 1;
+        
     }
 }
